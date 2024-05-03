e69546254aded90850c67eda2579d3b2c651e903
AceWalker001 <mwangangianthony1@gmail.com>
2024-05-03 14:44:21 +0300

    added create functionality

--- lib/yapper_web/controllers/yapper_live/yapper_blog.ex
+++ lib/yapper_web/controllers/yapper_live/yapper_blog.ex
@@ -4,10 +4,22 @@ defmodule YapperWeb.YapperBlogLive do
   alias Yapper.Post
 
   def render(assigns) do
+    posts = Repo.all(Post)
 
+    ~H"""
+      <div>
+        <%= for post <- posts do %>
+          <div>
+            <h1><%= post.title %></h1>
+            <p><%= post.content %></p>
+          </div>
+          <br>
+        <% end %>
+      </div>
+    """
   end
 
-  def handle_event("create", %{"title" => title, "content" => content}) do
+  def handle_event("create", %{"title" => title, "content" => content}, socket) do
     case Repo.insert(%Post{title: title, content: content})do
       {:ok, _post} ->
         {:noreply, socket}
